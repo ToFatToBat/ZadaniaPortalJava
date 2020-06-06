3494a14515d12936cd06ae4c98f6cb3e6acde59c
ToFatToBat <kpapiernia@wp.pl>
2020-06-06 09:12:01 +0200

    Initial commit

---
+++ src/zmienneITypyDanych/DataTypes.java
@@ -0,0 +1,10 @@
+package zmienneITypyDanych;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int x = 10;
+        double y = 4;
+        double div = x / y;
+        System.out.println(div);
+    }
+}
---
+++ src/zmienneITypyDanych/Kot.java
@@ -0,0 +1,14 @@
+package zmienneITypyDanych;
+
+public class Kot {
+    public static void main(String[] args) {
+        char a = 75;
+        char b = 79;
+        char c = 84;
+
+        System.out.print(a);
+        System.out.print(b);
+        System.out.print(c);
+
+    }
+}
